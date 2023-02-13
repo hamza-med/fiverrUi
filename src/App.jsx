1daf3977741ab5859a0302bf1e0193cd468689ed
Hamzamed <hamzaa.mohamed1998@gmail.com>
2023-02-13 20:27:23 +0100

    add navbar styling and boilerplate pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,72 @@
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import React from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import Gigs from "./pages/Gigs";
+import Gig from "./pages/Gig";
+import Orders from "./pages/Orders";
+import MyGigs from "./pages/MyGigs";
+import Add from "./pages/Add";
+import Messages from "./pages/Messages";
+import Message from "./pages/Message";
 const App = () => {
-  return <div>hello</div>;
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/mygigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message/:id",
+          element: <Message />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 
 export default App;
